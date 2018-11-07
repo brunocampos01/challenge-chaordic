c53d1add89821ccff409acee9cf04e118c1b1453
brunocampos01 <brunocampos01@gmail.com>
2018-11-07 12:08:00 -0200

    update

--- parte01-api.py
+++ parte01-api.py
@@ -1,16 +1,13 @@
 from flask import Flask, request
 from flask_api import FlaskAPI, status
-from time import sleep
 import datetime
 
-# create the flask object
+# create flask object
 app = Flask(__name__)
 
 # saved json and time
 datas_registered = []
 times_registered = []
-datas_split = []
-times_split = []
 
 @app.route('/chaordic.com.br/v1/products/', methods=['POST'])
 def post_route():
@@ -19,36 +16,31 @@ def post_route():
         data = request.get_json(force=True)
         # get time 
         minute = datetime.datetime.now().minute
-        
-        # 
-        buffer_10min()
 
-        if data in datas_split:
-            return f"{status.HTTP_403_FORBIDDEN} Forbidden\n "
+        # se times_split empty 
+        if len(times_registered) == 0:
+            times_registered.append(minute)
+            datas_registered.append(data)
+            return f"{status.HTTP_201_CREATED} OK\ndatas_split={datas_registered}\n\ntimes_split={times_registered}\n"
+        
+        # get diferença de tempo, entre o ultimo elemento na lista com o min atual
+        diferenca = minute - times_registered[-1]
+
+        '''
+        Ainda não é a melhor solução, neste caso esta havendo o bloqueio da última transação
+        e não somente da request que tem um mesmo json
+        '''
+
+        if data in datas_registered and diferenca < 1 :
+            return f"{status.HTTP_403_FORBIDDEN} Forbidden\n"
+        elif data in datas_registered and diferenca > 1:
+            datas_registered.append(data)
+            times_registered.append(minute)
+            return f"{status.HTTP_201_CREATED} OK\n\ndatas_split={datas_registered}\n\ntimes_split={times_registered}\n"
         else:
             datas_registered.append(data)
             times_registered.append(minute)
-        return f"{status.HTTP_201_CREATED} OK\ndatas={datas_registered}\ndatas_split={datas_split}\ntimes={times_registered}\n"
-
-
-def buffer_10min():
-    # percorre times_registered
-    for i in times_registered: 
-        index_decr = (len(times_registered) - times_registered.index(i)) - 1 # -1 pois index começa com 0
-        elem_atual = times_registered[index_decr]
-        elem_ultimo = times_registered[-1]
-        diferenca = elem_ultimo - elem_atual
-
-        # ultimos 10 min 
-        if diferenca < 1:
-            times_split.append(elem_atual)
-            datas_split.append(datas_registered[index_decr])
-        else:
-            times_split.pop(index_decr)
-            datas_split.pop(index_decr)
-            break # garante que não vai comparar todos os elementos
-                   
-
+        return f"{status.HTTP_201_CREATED} OK\n\ndatas_split={datas_registered}\n\ntimes_split={times_registered}\n"
 
 
 if __name__ == "__main__":
