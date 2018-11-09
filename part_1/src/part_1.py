a0ab0653e5b370063ed98bc7642172ce8219600f
brunocampos01 <brunocampos01@gmail.com>
2018-11-09 01:50:23 -0200

    fix: correções no bloqueio de requests

--- part_1/src/part_1.py
+++ part_1/src/part_1.py
@@ -17,29 +17,23 @@ def post_route():
         # get time 
         minute = datetime.datetime.now().minute
 
-        # se times_split empty 
-        if len(times_registered) == 0:
-            times_registered.append(minute)
-            datas_registered.append(data)
-            return f"{status.HTTP_201_CREATED} OK\n"
-        
-        # get diferença de tempo, entre o ultimo elemento na lista com o minute atual
-        difference = minute - times_registered[-1]
-        '''
-        Ainda não é a melhor solução, neste caso esta havendo o bloqueio da última transação
-        e não somente da request que tem um mesmo json
-        '''
-        if data in datas_registered and difference < 10 :
-            return f"{status.HTTP_403_FORBIDDEN} Forbidden\n"
-        elif data in datas_registered and difference > 10:
-            datas_registered.append(data)
-            times_registered.append(minute)
-            return f"{status.HTTP_201_CREATED} OK\n"
+        if data in datas_registered:
+            # armazena a posição na lista do data, quando ele foi armazenado pela última vez
+            position = len(datas_registered)-datas_registered[::-1].index(data)-1
+            # diferença entre o minuto atual e o minuto da ultima vez que esse dado apareceu na lista
+            difference = minute - times_registered[position]
+            
+            if difference <=10:
+                print('Forbidden')
+                return f"{status.HTTP_403_FORBIDDEN} Forbidden\n\n{times_registered}\n{datas_registered}\nposition_data={position}\nminute={minute}\ndifference={difference}\n" 
+            else:
+                datas_registered.append(data)
+                times_registered.append(minute)
+                return f"{status.HTTP_201_CREATED} OK\n (difference > 1)\n\n{times_registered}\n{datas_registered}\nposition_data={position}\nminute={minute}\ndifference={difference}\n"
         else:
             datas_registered.append(data)
             times_registered.append(minute)
-        return f"{status.HTTP_201_CREATED} OK\n"
-
+        return f"{status.HTTP_201_CREATED} OK\n (data not in)\n{times_registered}\n{datas_registered}\n"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
