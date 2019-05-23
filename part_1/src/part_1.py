4666f577de720f853bf89b3ba1da324fbcf2c7a9
brunocampos01 <brunocampos01@gmail.com>
2019-05-22 23:39:57 -0300

    refactor

--- part_1/src/part_1.py
+++ part_1/src/part_1.py
@@ -1,6 +1,7 @@
 import datetime
+
 from flask import Flask, request
-from flask_api import FlaskAPI, status
+from flask_api import status
 
 # flask object
 app = Flask(__name__)
@@ -8,6 +9,7 @@ app = Flask(__name__)
 # saved json and time
 datas_registered = []
 times_registered = []
+
 
 @app.route('/chaordic.com.br/v1/products/', methods=['POST'])
 def post_route():
@@ -19,13 +21,13 @@ def post_route():
 
         if data in datas_registered:
             # armazena a posição na lista do data, quando ele foi armazenado pela última vez
-            position = len(datas_registered)-datas_registered[::-1].index(data)-1
+            position = len(datas_registered) - datas_registered[::-1].index(data) - 1
             # diferença entre o minuto atual e o minuto da ultima vez que esse dado apareceu na lista
             difference = minute - times_registered[position]
-            
-            if difference <=10:
+
+            if difference <= 10:
                 print('Forbidden')
-                return f"{status.HTTP_403_FORBIDDEN} Forbidden\n" 
+                return f"{status.HTTP_403_FORBIDDEN} Forbidden\n"
             else:
                 datas_registered.append(data)
                 times_registered.append(minute)
@@ -34,6 +36,7 @@ def post_route():
             datas_registered.append(data)
             times_registered.append(minute)
         return f"{status.HTTP_201_CREATED} OK\n"
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
--- part_1/test/tests.py
+++ part_1/test/tests.py
@@ -1,9 +1,10 @@
 import unittest
+
 import requests
 from flask import Flask
-import test # faz as importações de módulos
 
 part_1 = Flask('part_1')
+
 
 class Test(unittest.TestCase):
     def setUp(self):
@@ -13,10 +14,11 @@ class Test(unittest.TestCase):
     def test_request(self):
         # envia uma requisicao POST
         result = requests.post('http://localhost:5000/chaordic.com.br/v1/products/',
-            json={"id": "123", "name": "mesa"})    
+                               json={"id": "123", "name": "mesa"})
         self.assertEqual(result.status_code, 200)
 
+
 if __name__ == '__main__':
-    print ('INICIANDO OS TESTES')
-    print(70*'-')
-    unittest.main(verbosity=2) # verbosity=2, tras mais informação na saída, sobre a função
+    print('INICIANDO OS TESTES')
+    print(70 * '-')
+    unittest.main(verbosity=2)  # verbosity=2, tras mais informação na saída, sobre a função
